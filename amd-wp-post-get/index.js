08a74991dd4de44de9bc6ebebdf204416421e3e9
jinmayamashita <jinmayamashita@gmail.com>
2018-10-02 08:06:15 +0900

    get list for tablename, get item for id

--- amd-wp-post-get/index.js
+++ amd-wp-post-get/index.js
@@ -1,14 +1,30 @@
-const ApiBuilder = require('claudia-api-builder');
-const AWS = require('aws-sdk');
+const ApiBuilder = require("claudia-api-builder");
+const AWS = require("aws-sdk");
 
 const api = new ApiBuilder();
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-api.get('/amd_works', function(request) {
+// get list for {tableName}
+api.get("/{tableName}", function(request) {
+  const tableName = request.pathParams.tableName;
   return dynamoDb
-    .scan({ TableName: 'amd_works' })
+    .scan({ TableName: tableName })
     .promise()
     .then(response => response.Items);
+});
+
+// get item for {id}
+api.get("/{tableName}/{id}", function(request) {
+  const id = request.pathParams.id;
+  return dynamoDb
+    .scan({
+      TableName: tableName,
+      Key: {
+        id: id
+      }
+    })
+    .promise()
+    .then(response => response.Item);
 });
 
 module.exports = api;
