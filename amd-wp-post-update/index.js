a755b5de09ee2da5e4bd08f5ec3e28f8c660e636
jinmayamashita <jinmayamashita@gmail.com>
2018-10-02 08:06:29 +0900

    added test code

--- amd-wp-post-update/index.js
+++ amd-wp-post-update/index.js
@@ -1,43 +1,23 @@
-const ApiBuilder = require('claudia-api-builder');
-const AWS = require('aws-sdk');
+const ApiBuilder = require("claudia-api-builder");
+const AWS = require("aws-sdk");
 
 const api = new ApiBuilder();
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 api.post(
-  '/amd_works',
-  function (request) {
-    let params = {
-      TableName: 'amd_works',
-      Item: {
-        ...request.body
-      }
-    };
-    return dynamoDb.put(params).promise();
-  },
-  { success: 201 }
-);
-
-
-api.post(
-  '/amd_news',
-  function (request) {
-    let params = {
-      TableName: 'amd_news',
-      Item: {
-        ...request.body
-      }
-    };
-    return dynamoDb.put(params).promise();
+  "/amd_test",
+  function(request) {
+    console.log("test success");
+    return;
   },
   { success: 201 }
 );
 
 api.post(
-  '/amd_projects',
+  "/amd_works",
   function(request) {
     let params = {
-      TableName: 'amd_projects',
+      TableName: "amd_works",
       Item: {
         ...request.body
       }
@@ -48,10 +28,10 @@ api.post(
 );
 
 api.post(
-  '/amd_people',
+  "/amd_news",
   function(request) {
     let params = {
-      TableName: 'amd_people',
+      TableName: "amd_news",
       Item: {
         ...request.body
       }
@@ -62,10 +42,38 @@ api.post(
 );
 
 api.post(
-  '/amd_recruitments',
+  "/amd_projects",
   function(request) {
     let params = {
-      TableName: 'amd_recruitments',
+      TableName: "amd_projects",
+      Item: {
+        ...request.body
+      }
+    };
+    return dynamoDb.put(params).promise();
+  },
+  { success: 201 }
+);
+
+api.post(
+  "/amd_people",
+  function(request) {
+    let params = {
+      TableName: "amd_people",
+      Item: {
+        ...request.body
+      }
+    };
+    return dynamoDb.put(params).promise();
+  },
+  { success: 201 }
+);
+
+api.post(
+  "/amd_recruitments",
+  function(request) {
+    let params = {
+      TableName: "amd_recruitments",
       Item: {
         id: request.body.id,
         title: request.body.title
